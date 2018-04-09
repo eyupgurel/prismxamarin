b0706f6b13a05eec34b897505196963bab084920
eyupgurel <eyupgurel@gmail.com>
2018-04-09 18:40:24 +0300

    initial commit

---
+++ PrismXamarin/PrismXamarin/Interface/IGitHubApi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Refit;
+using PrismXamarin.Model;
+
+namespace PrismXamarin.Interface
+{
+    [Headers("User-Agent: :request:")]
+    interface IGitHubApi
+    {
+        [Get("/search/users?q=location:london")]
+        IObservable<ApiResponse> GetLondonUsers();
+        [Get("/search/users?q=location:istanbul")]
+        IObservable<ApiResponse> GetIstanbulUsers();
+    }
+}
---
+++ PrismXamarin/PrismXamarin/Views/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using PrismXamarin.Interface;
+using Refit;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using PrismXamarin.Model;
+using System.Reactive.Linq;
+using System.Threading;
+
+namespace PrismXamarin.Views
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage ()
+		{
+                InitializeComponent();
+                var listView = new ListView();
+                List<User> users = new List<User>();
+                List<String> user_names = new List<String>();
+
+                JsonConvert.DefaultSettings = () => new JsonSerializerSettings()
+                {
+                    ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                    Converters = { new StringEnumConverter() }
+                };
+
+                IGitHubApi gitHubApi = RestService.For<IGitHubApi>("https://api.github.com");
+
+                IObservable<ApiResponse> istanbulUsers = gitHubApi.GetIstanbulUsers();
+
+                istanbulUsers.ObserveOn(SynchronizationContext.Current).Subscribe(resp =>
+                    {
+                        users = resp.items;
+                        foreach (User user in users)
+                        {
+                            user_names.Add(user.ToString());
+                        }
+                        listView.ItemsSource = user_names;
+                        Padding = new Thickness(0, 20, 0, 0);
+                        Content = listView;
+                    },
+                    ex => {
+                        string error = ex.Message;
+                    },
+                    () => { }
+                );
+        }
+	}
+}
